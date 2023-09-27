d2f4dd3b2edbca5e36599644175c971d23f66b9e
NitheeshKumarThangaraj <nitheeshkumar.t@syncfusion.com>
2023-09-27 12:13:09 +0530

    845168 Prepare a sample for Radialbar series blog in Maui

---
+++ RadialbarSample/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace RadialbarSample;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ RadialbarSample/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Syncfusion.Maui.Charts;
+
+namespace RadialbarSample;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void DataPointSelectionBehavior_SelectionChanged(object sender, Syncfusion.Maui.Charts.ChartSelectionChangedEventArgs e)
+    {
+        var series = sender as RadialBarSeries;
+
+        if (series != null)
+        {
+            int selectedIndex = series.SelectionBehavior.SelectedIndex;
+
+            if (selectedIndex < new PowerfulPassportViewModel().MostData.Count)
+            {
+                string selectedCountry = new PowerfulPassportViewModel().MostData[selectedIndex].Country;
+                double freeAccess = new PowerfulPassportViewModel().MostData[selectedIndex].Access;
+                StackLayout layout = new StackLayout();
+
+                var label = new Label { Text = "Free Access", TextColor = Colors.Black, HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Fill,Margin=new Thickness(0,20,0,0),FontAttributes= FontAttributes.Bold}; 
+                var label1 = new Label { Text = "Permitted by", Margin=new Thickness(10,0,0,0), FontAttributes = FontAttributes.Bold };
+                var label2 = new Label { Text = selectedCountry + " : " + freeAccess,Margin= new Thickness(10, 0, 0, 0), FontAttributes = FontAttributes.Bold };
+                layout.SetBinding(HeightRequestProperty, nameof(RadialBarSeries.CenterHoleSize));
+                layout.SetBinding(WidthRequestProperty, nameof(RadialBarSeries.CenterHoleSize));
+                layout.Background = Colors.White;
+                layout.Children.Add(label);
+                layout.Children.Add(label1);
+                layout.Children.Add(label2);
+                series.CenterView = layout;
+            }
+        }
+    }
+
+}
+
